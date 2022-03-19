cf752ce930fcd97c094e8ec6bed4748c9961ccd8
Mmmr7 <ol-sidorenkov@mail.ru>
2022-03-19 23:14:58 +0300

    Bug fixing

--- app/src/main/java/com/example/weatherapp/ApiClientImplementation.java
+++ app/src/main/java/com/example/weatherapp/ApiClientImplementation.java
@@ -1,12 +1,23 @@
 package com.example.weatherapp;
 
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+import okhttp3.logging.HttpLoggingInterceptor;
 
 public class ApiClientImplementation {
 
     private static Retrofit retro;
     private static OkHttpClient client;
+
+    private static final int REQUEST_TIMEOUT = 60;
 
 
     public static Retrofit retrofitBuilder() {
@@ -26,18 +37,23 @@ public class ApiClientImplementation {
                     .connectTimeout(REQUEST_TIMEOUT, TimeUnit.SECONDS)
                     .readTimeout(REQUEST_TIMEOUT, TimeUnit.SECONDS)
                     .writeTimeout(REQUEST_TIMEOUT, TimeUnit.SECONDS);
-
-            var interceptor = new HttpLoggingInterceptor();
+            // https://stackoverflow.com/questions/42835315/httplogginginterceptor-for-http-request-response-logging
+            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
             interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
             httpClient.addInterceptor(new Interceptor() {
                 @Override
-                public Response intercept(Interceptor.Chain chain) {
-                    Request original = new chain.
+                public Response intercept(Interceptor.Chain chain) throws IOException {
+                    Request original = chain.request();
+                    Request.Builder requestBuilder = original.newBuilder()
+                            .addHeader("Accept", "application/json")
+                            .addHeader("Content-Type", "application/json");
+                    // Request request = requestBuilder.build();
+                    return chain.proceed(requestBuilder.build());
                 }
-            })
+            });
+            client = httpClient.build();
         }
-
+        return retro;
     }
-
 }
--- app/src/main/java/com/example/weatherapp/Constants.java
+++ app/src/main/java/com/example/weatherapp/Constants.java
@@ -2,6 +2,8 @@ package com.example.weatherapp;
 
 public class Constants {
 
+    
+
     public static final String KEY_API = "773f4e183970a3c6e9ff35fe7a6b7fe3";
 
     public static final String LANGUAGE = "en-EN";
